93fbdcdb3fb7cd85e3fcdb3c1c66fdf10cd0df98
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2022-10-20 02:16:30 +0530

    added header structure

---
+++ src/components/nav/nav.tsx
@@ -0,0 +1,13 @@
+export const Nav: React.FC = () => {
+  return (
+    <nav className="nav">
+      <button className="btn nav-btn">Get the App</button>
+      <ul>
+        <li>Investor Relations</li>
+        <li>Add restaurant</li>
+        <li>Log in</li>
+        <li>Sign up</li>
+      </ul>
+    </nav>
+  );
+};
