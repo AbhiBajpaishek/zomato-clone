1d74733475faddf3456455f73abb3bbd25f86af2
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2022-11-02 23:42:52 +0530

    Fixed search bar and header bg img

--- src/components/searchbar/searchBar.tsx
+++ src/components/searchbar/searchBar.tsx
@@ -2,7 +2,10 @@ import './searchBar.css';
 export const SearchBar: React.FC = () => {
   return (
     <div className="search-bar">
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search for restaurant, cuisine or a dish"
+      />
     </div>
   );
 };
