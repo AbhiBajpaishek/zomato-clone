9f7fba39589aae3c4bea5ca6937844ac44e21b22
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2022-11-08 16:38:52 +0530

    Work responsive page in progress

--- src/components/collections/Collections.tsx
+++ src/components/collections/Collections.tsx
@@ -7,30 +7,32 @@ import './Collections.css';
 export const Collections: React.FC = () => {
   return (
     <div className="collections">
-      <h1 className="collections-title">Collections</h1>
-      <div className="collections-info">
-        <h5 className="collections-info_text">
-          Explore curated lists of top restaurants, cafes, pubs, and bars in
-          Lucknow, based on trends
-        </h5>
-        <a href="index.html" className="collections-info_button">
-          All Collections in Lucknow
-          <i className="sc-rbbb40-1 iFnyeo sc-bFADNz fskncn" color="#FF7E8B">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="#FF7E8B"
-              width="12"
-              height="12"
-              viewBox="0 0 20 20"
-              aria-labelledby="icon-svg-title- icon-svg-desc-"
-              role="img"
-              className="sc-rbbb40-0 ezrcri"
-            >
-              <title>right-triangle</title>
-              <path d="M5 0.42l10 10-10 10v-20z"></path>
-            </svg>
-          </i>
-        </a>
+      <div className="collection-header">
+        <h1 className="collections-title">Collections</h1>
+        <div className="collections-info">
+          <h5 className="collections-info_text">
+            Explore curated lists of top restaurants, cafes, pubs, and bars in
+            Lucknow, based on trends
+          </h5>
+          <a href="index.html" className="collections-info_button">
+            All Collections in Lucknow
+            <i className="sc-rbbb40-1 iFnyeo sc-bFADNz fskncn" color="#FF7E8B">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#FF7E8B"
+                width="12"
+                height="12"
+                viewBox="0 0 20 20"
+                aria-labelledby="icon-svg-title- icon-svg-desc-"
+                role="img"
+                className="sc-rbbb40-0 ezrcri"
+              >
+                <title>right-triangle</title>
+                <path d="M5 0.42l10 10-10 10v-20z"></path>
+              </svg>
+            </i>
+          </a>
+        </div>
       </div>
       <div className="collection-cards">
         <ImageCard
