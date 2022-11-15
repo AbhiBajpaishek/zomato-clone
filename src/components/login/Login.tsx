c4b9eaee423a1e59baadcd2b1f25fc59b81e5606
AbhiBajpaishek <abhishekbajpai449@gmailcom>
2022-11-15 22:38:48 +0530

    UI improvement in login screen

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -5,10 +5,15 @@ import './Login.css';
 export const Login: React.FC = () => {
   return (
     <div className="login-panel">
-      <h4>Login</h4>
+      <h4 className="login-head">Login</h4>
       <section className="login-panel_main">
-        <InputText type="number" placeholder={'Phone'}></InputText>
-        <Button text="Continue With Email">
+        <InputText
+          type="number"
+          className="login-number"
+          placeholder={'Phone'}
+        ></InputText>
+        <Button text=""> Send One Time Password</Button>
+        <Button className="login-btn" text="">
           <i className="sc-rbbb40-1 fDHLHG" color="#EF4F5F">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -18,16 +23,17 @@ export const Login: React.FC = () => {
               viewBox="0 0 20 20"
               aria-labelledby="icon-svg-title- icon-svg-desc-"
               role="img"
-              className="sc-rbbb40-0 cdktuZ"
+              className="sc-rbbb40-0 cdktuZ icon"
             >
               <title>mail-fill</title>
               <path d="M10 9.58c-1.62 0-10-4.76-10-4.76v-0.74c0-0.92 0.74-1.66 1.66-1.66h16.68c0.92 0 1.66 0.74 1.66 1.66l-0.020 0.84c0 0-8.28 4.66-9.98 4.66zM10 11.86c1.78 0 9.98-4.46 9.98-4.46l0.020 10c0 0.92-0.74 1.66-1.66 1.66h-16.68c-0.92 0-1.66-0.74-1.66-1.66l0.020-10c0 0 8.36 4.46 9.98 4.46z"></path>
             </svg>
           </i>
-          <span>Continue With Email</span>
+          <span className="email-span">Continue With Email</span>
         </Button>
-        <Button text="">
+        <Button className="login-btn" text="">
           <svg
+            className="icon"
             width="24"
             height="24"
             viewBox="0 0 22 22"
@@ -51,9 +57,8 @@ export const Login: React.FC = () => {
               fill="#F14336"
             ></path>
           </svg>
-          <span>Continue With Google</span>
+          <span className="gmail-span">Continue With Google</span>
         </Button>
-        <Button text=""> Send One Time Password</Button>
       </section>
     </div>
   );
